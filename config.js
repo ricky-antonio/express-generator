6395978e2e58f106f6b838c89c2ad3c5aeac25f6
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-06-03 16:16:43 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '325884915616837',
+        clientSecret: '9d3161e41f0881082c077d0d0b157e6d'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,12 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
 	User.find()
-            .then(users => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(users);
-            })
-            .catch(err => next(err));
+		.then(users => {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(users);
+		})
+		.catch(err => next(err));
 	// res.send('respond with a resource');
 });
 
@@ -71,6 +71,15 @@ router.get('/logout', cors.corsWithOptions, (req, res, next) => {
 		const err = new Error('You are not logged in!');
 		err.status = 401;
 		return next(err);
+	}
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+	if (req.user) {
+		const token = authenticate.getToken({_id: req.user._id});
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({success: true, token:token, status: 'You are succesfully logged in!'});
 	}
 });
 
